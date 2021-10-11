b735f153f8a040b716785ffc4e5f533d3d7ed5cd
Daniel Antonio <danielmarinantonio@gmail.com>
2021-10-10 19:04:27 -0500

    Carga de proyecto

---
+++ dxdx/dxdx.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "b496d6f7-f05d-4a95-a3c3-3387d2f0408d",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
---
+++ dxdx/dxdx/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace dxdx
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Gato.IsVisible = true;
+            btnVer.IsVisible = false;
+            DisplayAlert("❤️", "Gracias por ver al Mishi de la Suerte", "💙💚💙Aceptar");
+
+            DisplayAlert("Sorpresa", "Acabas de ver al mishi de la suerte 🐱", "Gracias!");
+        }
+    }
+}
